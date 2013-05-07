36698d15dea59168a1c4dcd39ea0a6821fc2ec7c
acumulador <jcsalazarm@gmail.com>
2013-05-07 11:28:17 -0500

    Cambios comentarios para codigo js

--- js/myCod.js
+++ js/myCod.js
@@ -5,12 +5,14 @@ function ini()
 	$("#elUL").on("click",detecta);
 }
 
+//esta función detecta el id del Li y lo muestra en alert
 function detecta(e)
 {
+	//con target capturo el id y lo almaceno en variable
 	var myLi = e.target.id;
+	//Valido si es el padre (ul) o el hijo (li)
 	if( myLi != "elUL" )
 	{
 		alert("Dio click en: " + myLi); //nodeName;
 	}
-	// alert("Bien!!");
 }
